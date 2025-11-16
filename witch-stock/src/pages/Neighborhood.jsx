4fdbb1c501b0115e7452ebca073f0ed0e2446f37
Illusioner <tomo701may@gmail.com>
2025-11-16 02:35:48 -0500

    position neighborhood elements

--- witch-stock/src/pages/Neighborhood.jsx
+++ witch-stock/src/pages/Neighborhood.jsx
@@ -6,7 +6,12 @@ function Neighborhood () {
     <>
     <body className="neighborhood">
         <p>neighborhood</p>
-        <Link to="/Person/dkjs"><img className="BondHouse" src="../src/assets/House.PNG"></img></Link>
+        <Link to="/Person/jamesbond"><img className="BondHouse" src="../src/assets/House.PNG"></img></Link>
+        <Link to="/Person/jackstock"><img className="StockHouse" src="../src/assets/House.PNG"></img></Link>
+        <Link to="/Person/ETF"><img className="ETFHouse" src="../src/assets/House.PNG"></img></Link>
+        <Link to="/Person/home"><img className="MyHouse" src="../src/assets/House.PNG"></img></Link>
+        <Link to="/Person/commodity"><img className="CommodityHouse" src="../src/assets/House.PNG"></img></Link>
+        <Link to="/Person/cd"><img className="CdHouse" src="../src/assets/House.PNG"></img></Link>
     </body>
     </>
     );
